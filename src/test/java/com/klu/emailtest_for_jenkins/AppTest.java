94c4fb65dc8552e18b25eb4e37cc49d188cec9b1
Durgarao <dgunja390@gmail.com>
2025-01-24 09:23:05 +0530

    initial commit

---
+++ src/test/java/com/klu/emailtest_for_jenkins/AppTest.java
@@ -0,0 +1,19 @@
+package com.klu.emailtest_for_jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
